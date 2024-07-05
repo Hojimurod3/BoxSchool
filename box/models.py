880e5438200445473cb5e0dd24f6b6e67a3ae280
Hojimurod3 <147169978+Hojimurod3@users.noreply.github.com>
2024-07-05 16:34:52 +0500

    feat: create config

--- box/models.py
+++ box/models.py
@@ -1,19 +1,19 @@
 from django.db import models
+from config.models import Time
 
-
-class Video(models.Model):
+class Video(Time):
     teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE, related_name="video")
     video = models.FileField()
 
 
-class Teacher(models.Model):
+class Teacher(Time):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     age = models.CharField(max_length=100)
     information = models.TextField()
 
 
-class Post(models.Model):
+class Post(Time):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="post")
     title = models.CharField(max_length=255)
     information = models.TextField()
